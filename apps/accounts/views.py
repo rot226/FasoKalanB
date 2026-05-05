b97962e7c04b99c742778b2d6a35adeccbb3e9d3
rot226 <160727202+rot226@users.noreply.github.com>
2026-05-05 13:03:54 +0000

    Configure routing with app-level URL includes

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def login_view(request):
+    return HttpResponse("Connexion")
+
+
+def logout_view(request):
+    return HttpResponse("Déconnexion")
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
+    path("accounts/", include("apps.accounts.urls")),
+    path("dashboard/", include("apps.dashboard.urls")),
 ]
